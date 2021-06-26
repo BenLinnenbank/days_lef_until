aebc94225be9714a6228033cd3e90f8c4fb6be15
Ben <Ben@Air-van-Ben.fritz.box>
2021-06-26 20:05:38 +0200

    Add basic layout, Add fetch to get random GIF from Giphy, Add calculations until we are going to texel

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysLeftContainer = document.querySelector('#days-left-container');
+const gif = document.querySelector('#gif');
+
+const calculateDaysLeft = () => {
+    const date1 = new Date();
+    const date2 = new Date("08/06/2021");
+
+    const differenceInTime = date2.getTime() - date1.getTime();
+
+    const differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
+
+    return differenceInDays.toString();
+}
+
+daysLeftContainer.innerHTML = `Only ${calculateDaysLeft()} days left!!!`;
+
+const getRandomPartyGIF = async () => {
+    const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=4ioDDWCUu8WJPtzMSNtPxCdQQLlDWoff&tag=excited&rating=g');
+
+    const randomPartyGIFObj = await response.json();
+
+    console.log(randomPartyGIFObj);
+
+    gif.src = await randomPartyGIFObj.data.images.original.url;
+    gif.style.maxWidth = `${randomPartyGIFObj.data.images.original.width}`;
+    gif.style.maxHeight = `${randomPartyGIFObj.data.images.original.height}`;
+
+}
+
+getRandomPartyGIF();
